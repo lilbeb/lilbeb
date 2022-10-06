7f9897b1c8342496cfa5ae369439ca15ebf31577
lilbeb <66528840+lilbeb@users.noreply.github.com>
2022-10-06 20:46:00 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,5 @@
+import string
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -10,7 +12,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    ind = 0
+    for i in plaintext:
+        if i.islower():
+            cur = ord(keyword[ind]) - ord('a')
+            enc = chr((ord(i) - ord('a') + cur) % 26 + ord('a'))
+            ciphertext += enc
+            ind = (ind + 1) % len(keyword)
+        elif i.isupper():
+            cur = ord(keyword[ind]) - ord('A')
+            enc = chr((ord(i) - ord('A') + cur) % 26 + ord('A'))
+            ciphertext += enc
+            ind = (ind + 1) % len(keyword)
+        else:
+            ciphertext += i
+
     return ciphertext
 
 
@@ -26,5 +43,30 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    ind = 0
+
+    for i in ciphertext:
+        if i.islower():
+            cur = ord(keyword[ind]) - ord('a')
+            dec = ord(i) - ord('a') - cur
+            if dec < 0:
+                dec += 26
+
+            decc = chr(dec + ord('a'))
+            plaintext += decc
+            ind = (ind + 1) % len(keyword)
+
+        elif i.isupper():
+            cur = ord(keyword[ind]) - ord('A')
+            dec = ord(i) - ord('A') - cur
+            if dec < 0:
+                dec += 26
+
+            decc = chr(dec + ord('A'))
+            plaintext += decc
+            ind = (ind + 1) % len(keyword)
+        else:
+            plaintext += i
+
     return plaintext
