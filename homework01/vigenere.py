b973e25ec8579f8e204d72ffae3fac720bae1010
lilbeb <66528840+lilbeb@users.noreply.github.com>
2022-10-12 21:53:01 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -17,13 +17,13 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     ind = 0
     for i in plaintext:
         if i.islower():
-            cur = ord(keyword[ind]) - ord('a')
-            enc = chr((ord(i) - ord('a') + cur) % 26 + ord('a'))
+            cur = ord(keyword[ind]) - ord("a")
+            enc = chr((ord(i) - ord("a") + cur) % 26 + ord("a"))
             ciphertext += enc
             ind = (ind + 1) % len(keyword)
         elif i.isupper():
-            cur = ord(keyword[ind]) - ord('A')
-            enc = chr((ord(i) - ord('A') + cur) % 26 + ord('A'))
+            cur = ord(keyword[ind]) - ord("A")
+            enc = chr((ord(i) - ord("A") + cur) % 26 + ord("A"))
             ciphertext += enc
             ind = (ind + 1) % len(keyword)
         else:
@@ -49,22 +49,22 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
 
     for i in ciphertext:
         if i.islower():
-            cur = ord(keyword[ind]) - ord('a')
-            dec = ord(i) - ord('a') - cur
+            cur = ord(keyword[ind]) - ord("a")
+            dec = ord(i) - ord("a") - cur
             if dec < 0:
                 dec += 26
 
-            decc = chr(dec + ord('a'))
+            decc = chr(dec + ord("a"))
             plaintext += decc
             ind = (ind + 1) % len(keyword)
 
         elif i.isupper():
-            cur = ord(keyword[ind]) - ord('A')
-            dec = ord(i) - ord('A') - cur
+            cur = ord(keyword[ind]) - ord("A")
+            dec = ord(i) - ord("A") - cur
             if dec < 0:
                 dec += 26
 
-            decc = chr(dec + ord('A'))
+            decc = chr(dec + ord("A"))
             plaintext += decc
             ind = (ind + 1) % len(keyword)
         else:
