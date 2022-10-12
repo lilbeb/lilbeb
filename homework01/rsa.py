abe045cf466a0b559356d55bdb67cba083b297bc
lilbeb <66528840+lilbeb@users.noreply.github.com>
2022-10-12 22:07:16 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -13,13 +13,14 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    if n < 2:
+    if n == 2 or n == 3: 
+        return True
+    if n % 2 == 0 or n < 2: 
         return False
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
-        else:
-            return True
+    return True
 
 
 def gcd(a: int, b: int) -> int:
