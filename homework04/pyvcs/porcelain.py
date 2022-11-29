1a89e66daf991340c6c1fff014ee2136861aa8dc
lilbeb <66528840+lilbeb@users.noreply.github.com>
2022-11-29 23:26:47 +0300

    upd

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -4,7 +4,7 @@ import shutil
 import typing as tp
 
 from pyvcs.index import read_index, update_index
-from pyvcs.objects import commit_parse, find_tree_files, read_object
+from pyvcs.objects import commit_parse, find_object, find_tree_files, read_object
 from pyvcs.refs import get_ref, is_detached, resolve_head, update_ref
 from pyvcs.tree import commit_tree, write_tree
 
@@ -14,33 +14,33 @@ def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
-    ind = read_index(gitdir)
-    return commit_tree(gitdir, write_tree(gitdir, ind), message, author)
-
+    index = read_index(gitdir)
+    return commit_tree(
+        gitdir=gitdir, tree=write_tree(gitdir, index), message=message, author=author
+    )
 
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
-    file = gitdir / "refs" / "heads" / obj_name
-    if file.exists():
-        with open(file, "r") as f:
+    head = gitdir / "refs" / "heads" / obj_name
+    if head.exists():
+        with head.open("r") as f:
             obj_name = f.read()
-    ind = read_index(gitdir)
-    for element in ind:
-        if pathlib.Path(element.name).is_file():
-            if "/" in element.name:
-                pos = element.name.find("/")
-                shutil.rmtree(element.name[:pos])
+    index = read_index(gitdir)
+    for i in index:
+        if pathlib.Path(i.name).is_file():
+            if "/" in i.name:
+                shutil.rmtree(i.name[: i.name.find("/")])
             else:
-                os.chmod(element.name, 0o777)
-                os.remove(element.name)
-    path = gitdir / "objects" / obj_name[:2] / obj_name[2:]
-    with open(path, "rb") as f1:
-        tree = commit_parse(f1.read()).decode()
-    for el in find_tree_files(tree, gitdir):
-        if "/" in el[0]:
-            pos = el[0].find("/")
-            dir_name = el[0][:pos]
-            os.mkdir(dir_name)
-        with open(el[0], "w") as f:
-            _, content = read_object(el[1], gitdir)
-            f.write(content.decode())
+                os.chmod(i.name, 0o777)
+                os.remove(i.name)
+    obj_path = gitdir / "objects" / obj_name[:2] / obj_name[2:]
+    with obj_path.open("rb") as f:
+        com = f.read()
+    for j in find_tree_files(commit_parse(com).decode(), gitdir):
+        if "/" in j[0]:
+            dir_name = j[0][: j[0].find("/")]
+            pathlib.Path(dir_name).absolute().mkdir()
+
+        with open(j[0], "w") as file:
+            header, content = read_object(j[1], gitdir)
+            file.write(content.decode())
