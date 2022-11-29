dcef14d44db93ebdae319f55f67efac3d2e1378f
lilbeb <66528840+lilbeb@users.noreply.github.com>
2022-11-29 13:44:42 +0300

    upd

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -4,10 +4,30 @@ import typing as tp
 
 
 def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    # PUT YOUR CODE HERE
-    ...
+    gitdir = os.getenv("GIT_DIR", ".git")
+    cur = pathlib.Path(workdir)
+    path = cur / gitdir
+    if path.exists():
+        return path
+    for directory in path.parents:
+        if directory.name == gitdir:
+            return directory
+    raise Exception("Not a git repository")
 
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
-    # PUT YOUR CODE HERE
-    ...
+    workdir = pathlib.Path(workdir)
+    if not workdir.is_dir():
+        raise Exception(f"{workdir.name} is not a directory")
+    gitdir = os.getenv("GIT_DIR", ".git")
+    os.makedirs(workdir / gitdir / "refs" / "tags")
+    os.makedirs(workdir / gitdir / "objects")
+    os.makedirs(workdir / gitdir / "refs" / "heads")
+
+    (workdir / gitdir / "HEAD").write_text("ref: refs/heads/master\n")
+    (workdir / gitdir / "config").write_text(
+        "[core]\n\trepositoryformatversion = 0\n\tfilemode = true"
+        "\n\tbare = false\n\tlogallrefupdates = false\n"
+    )
+    (workdir / gitdir / "description").write_text("Unnamed pyvcs repository.\n")
+    return workdir / gitdir
