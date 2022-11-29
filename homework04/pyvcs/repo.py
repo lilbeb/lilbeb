4a1f20087bde5b228d92925da21e986d434aa3a8
lilbeb <66528840+lilbeb@users.noreply.github.com>
2022-11-29 23:27:41 +0300

    upd

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -4,30 +4,41 @@ import typing as tp
 
 
 def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    gitdir = os.getenv("GIT_DIR", ".git")
-    cur = pathlib.Path(workdir)
-    path = cur / gitdir
+    workdir = pathlib.Path(workdir)
+    if os.environ.get("GIT_DIR"):
+        git_dir = os.environ["GIT_DIR"]
+    else:
+        git_dir = ".git"
+    path = workdir / git_dir
     if path.exists():
         return path
-    for directory in path.parents:
-        if directory.name == gitdir:
-            return directory
-    raise Exception("Not a git repository")
+    else:
+        for i in path.parents:
+            if i.name == git_dir:
+                return i
+        raise Exception("Not a git repository")
 
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
     workdir = pathlib.Path(workdir)
     if not workdir.is_dir():
         raise Exception(f"{workdir.name} is not a directory")
-    gitdir = os.getenv("GIT_DIR", ".git")
-    os.makedirs(workdir / gitdir / "refs" / "tags")
-    os.makedirs(workdir / gitdir / "objects")
-    os.makedirs(workdir / gitdir / "refs" / "heads")
-
-    (workdir / gitdir / "HEAD").write_text("ref: refs/heads/master\n")
-    (workdir / gitdir / "config").write_text(
-        "[core]\n\trepositoryformatversion = 0\n\tfilemode = true"
-        "\n\tbare = false\n\tlogallrefupdates = false\n"
-    )
-    (workdir / gitdir / "description").write_text("Unnamed pyvcs repository.\n")
-    return workdir / gitdir
+    if os.environ.get("GIT_DIR"):
+        git_dir = os.environ["GIT_DIR"]
+    else:
+        git_dir = ".git"
+    path = workdir / git_dir
+    path.mkdir()
+    (path / "refs").mkdir()
+    (path / "refs/heads").mkdir()
+    (path / "refs/tags").mkdir()
+    (path / "objects").mkdir()
+    with open(path / "HEAD", "w") as head:
+        head.write("ref: refs/heads/master\n")
+    with open(path / "config", "w") as config:
+        config.write(
+            "[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = false\n"
+        )
+    with open(path / "description", "w") as description:
+        description.write("Unnamed pyvcs repository.\n")
+    return path
