31e45bccd6369d6ead1dc4a31ac03a80a62e8979
lilbeb <66528840+lilbeb@users.noreply.github.com>
2022-11-29 23:28:17 +0300

    upd

--- homework04/pyvcs/tree.py
+++ homework04/pyvcs/tree.py
@@ -1,33 +1,67 @@
 import os
 import pathlib
+import stat
+import time
 import typing as tp
 
-
-def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    gitdir = os.getenv("GIT_DIR", ".git")
-    cur = pathlib.Path(workdir)
-    path = cur / gitdir
-    if path.exists():
-        return path
-    for directory in path.parents:
-        if directory.name == gitdir:
-            return directory
-    raise Exception("Not a git repository")
+from pyvcs.index import GitIndexEntry, read_index
+from pyvcs.objects import hash_object
+from pyvcs.refs import get_ref, is_detached, resolve_head, update_ref
 
 
-def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
-    workdir = pathlib.Path(workdir)
-    if not workdir.is_dir():
-        raise Exception(f"{workdir.name} is not a directory")
-    gitdir = os.getenv("GIT_DIR", ".git")
-    os.makedirs(workdir / gitdir / "refs" / "tags")
-    os.makedirs(workdir / gitdir / "objects")
-    os.makedirs(workdir / gitdir / "refs" / "heads")
+def write_tree(gitdir: pathlib.Path, index: tp.List[GitIndexEntry], dirname: str = "") -> str:
+    data = b""
 
-    (workdir / gitdir / "HEAD").write_text("ref: refs/heads/master\n")
-    (workdir / gitdir / "config").write_text(
-        "[core]\n\trepositoryformatversion = 0\n\tfilemode = true"
-        "\n\tbare = false\n\tlogallrefupdates = false\n"
+    for file in index:
+        if "/" in file.name:
+            s = file.name.find("/")
+            directory = file.name[:s].encode()
+            mode = oct(file.mode)[2:].encode()
+            child = (
+                b""
+                + mode
+                + b" "
+                + file.name[file.name.find("/") + 1 :].encode()
+                + b"\0"
+                + file.sha1
+            )
+            hash = bytes.fromhex(hash_object(child, "tree", True))
+            data += b"40000 " + directory + b"\0" + hash
+        else:
+            data += oct(file.mode)[2:].encode() + b" " + file.name.encode() + b"\0" + file.sha1
+
+    return hash_object(data, fmt="tree", write=True)
+
+
+def commit_tree(
+    gitdir: pathlib.Path,
+    tree: str,
+    message: str,
+    parent: tp.Optional[str] = None,
+    author: tp.Optional[str] = None,
+) -> str:
+    zone = time.timezone
+    offset = "+" if zone < 0 else "-"
+    zone = abs(zone)
+    zone = zone // 60
+    offset += f"{zone // 60:02}"
+    offset += f"{zone % 60:02}"
+
+    local = time.localtime()
+    sec = time.mktime(local)
+    sec = int(sec)
+
+    if not author:
+        author = f"{os.getenv('GIT_AUTHOR_NAME')} <{os.getenv('GIT_AUTHOR_EMAIL')}>"
+
+    dt = [f"tree {tree}"]
+
+    if parent:
+        dt.append(f"parent {parent}")
+
+    dt.extend(
+        [f"author {author} {sec} {offset}", f"committer {author} {sec} {offset}", f"\n{message}\n"]
     )
-    (workdir / gitdir / "description").write_text("Unnamed pyvcs repository.\n")
-    return workdir / gitdir
+    string = "\n".join(dt)
+
+    return hash_object(string.encode(), "commit", True)
