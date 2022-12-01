075890b493574f87ba20866a31999a6a7b86896f
lilbeb <66528840+lilbeb@users.noreply.github.com>
2022-12-01 15:17:47 +0300

    age

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -1,17 +1,19 @@
 import datetime as dt
-import statistics
 import typing as tp
-
 from vkapi.friends import get_friends
 
 
 def age_predict(user_id: int) -> tp.Optional[float]:
-    """
-    Наивный прогноз возраста пользователя по возрасту его друзей.
-
-    Возраст считается как медиана среди возраста всех друзей пользователя
-
-    :param user_id: Идентификатор пользователя.
-    :return: Медианный возраст пользователя.
-    """
-    pass
+    friend = get_friends(user_id).items
+    age_now = dt.datetime.now().year
+    t = 0
+    age = 0
+    for i in friend:
+        try:
+            age += int(age_now - int(i["bdate"][5:]))
+            t += 1
+        except:
+            pass
+    if t > 0:
+        return age // t
+    return None
